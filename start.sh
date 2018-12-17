c483e2507de16b65d7082d74be1d7557d93c858d
Vlad <vladkyry@gmail.com>
2018-12-17 15:45:47 +0100

    Update start script

--- start.sh
+++ start.sh
@@ -8,13 +8,13 @@ pm2 start dist/app/server/main.js --name $APPLICATION_NAME
 # Check if application is running
 sleep 3
 
-RESTART=$(pm2 -m ls | grep "\-\- $APPLICATION_NAME" -A 6 | grep "restarted" | grep -oP "\d+$")
+RESTART=$(pm2 -m ls | grep "\-\- $APPLICATION_NAME" -A 6 | grep "restarted" | egrep -o "[0-9]+$")
 
-if [ "$RESTART" -ne 0 ]; then
+if [ "$RESTART" -eq 0 ]; then
+  echo "Application $APPLICATION_NAME is running."
+  exit 0
+else
   echo "Application $APPLICATION_NAME is not running."
   pm2 delete $APPLICATION_NAME
   exit 1
-else
-  echo "Application $APPLICATION_NAME is running."
-  exit 0
 fi
